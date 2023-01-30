b3187df55ca01841f4d1273d5f6b42f907170049
Jacinto Paulo Neto <83088426+jacintoneto@users.noreply.github.com>
2023-01-30 11:59:05 -0300

    Delete U1 - Python programs directory

--- U1 - Python programs/Capitulo2_Parte1/exercicio2_4.py
+++
@@ -1,8 +0,0 @@
-# Exercício 2.4 - Determinar se o número é ímpar ou par;
-
-n = int(input("Informe um valor inteiro: "))
-
-if(n%2==0):
-	print("O número", n, " é PAR!")
-else:
-	print("O número", n, " é ÍMPAR!")
--- U1 - Python programs/Capitulo2_Parte1/teste_list.py
+++
@@ -1,9 +0,0 @@
-#Programa que calcula a raiz quadrada de todos os elementos de uma lista
-
-from math import *
-
-r = [4,9,16,25]
-raizes = list(map(sqrt, r))
-print(r)
-print(raizes)
-	
